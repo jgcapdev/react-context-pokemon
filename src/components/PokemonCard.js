81855f541688a8c58ed4baddce9e054587323f94
jgcapdev <jorge.gomez-carrillo-external@capgemini.com>
2022-08-08 15:03:17 +0200

    fix: pokemon render card now works

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,21 +2,25 @@ const PokemonCard = ({ pokemon }) => {
   console.log(pokemon);
 
   return (
-    <div className="card text-center bg-primary">
-      {/* <img className="card-img-top" src={pokemon.sprites.front_default} alt="Card image cap" /> */}
-      <div className="card-body">
-        <h5 className="card-title">{pokemon.name}</h5>
+    pokemon && (
+      <div className="card text-center bg-primary">
+        <img className="card-img-top mx-auto w-25" src={pokemon.sprites.front_default} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{pokemon.name.toUpperCase()}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Weight: {pokemon.weight}</li>
+          <li className="list-group-item">Height: {pokemon.height}</li>
+        </ul>
+        <ul className="list-group list-group-flush">
+          {Object.keys(pokemon.abilities).map((ab) => (
+            <li className="list-group-item" key={ab}>
+              {pokemon.abilities[ab].ability.name}
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">Weight: {pokemon.weight}</li>
-        <li className="list-group-item">Height: {pokemon.height}</li>
-      </ul>
-      {/* <ul className="list-group list-group-flush">
-        {Object.keys(pokemon.abilities).map((ab) => (
-          <li key={ab}>{ab.ability}</li>
-        ))}
-      </ul> */}
-    </div>
+    )
   );
 };
 
